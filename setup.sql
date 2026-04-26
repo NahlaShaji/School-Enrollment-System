b7c5383ab4beb47cf5cf4e3d2221730bdf66d2a1
NahlaShaji <melissaarun9@gmail.com>
2026-04-26 23:30:50 +0530

    Delete Students table creation SQL statement
    Remove Students table creation from setup.sql

--- setup.sql
+++ setup.sql
@@ -1,8 +1,1 @@
-CREATE TABLE IF NOT EXISTS Students (
-    RollNo INTEGER PRIMARY KEY,
-    FullName TEXT NOT NULL,
-    Class TEXT NOT NULL,
-    BirthDate TEXT NOT NULL,
-    Address TEXT NOT NULL,
-    EnrollDate TEXT NOT NULL
-);
+
